f235185e3556159d0dd98481b9bf376cb2d7712e
kyleparrott <kyle.parrott@gmail.com>
2016-04-22 11:15:55 -0500

    initial putchar stub implementation for msp430-gcc

---
+++ msp430-gcc-putchar/putchar.h
@@ -0,0 +1,5 @@
+#ifndef MSP430_GCC_PUTCHAR_H
+#define MSP430_GCC_PUTCHAR_H
+
+int putchar(int character);
+
---
+++ source/putchar.c
@@ -0,0 +1,5 @@
+#include <msp430.h>
+
+int putchar(int character) {
+        return 1;
+}
